074edc095a84cd6aa03db4531a53b136f8444809
Ryan Cather <ryan.cather@ed.act.edu.au>
2022-10-05 01:36:01 +1100

    Bullet firing

--- Bullet.gd
+++ Bullet.gd
@@ -3,13 +3,16 @@ extends Area
 var speed : float = 30.0
 var damage : int = 1
 
-
 func _process (delta):
 	# move the bullet forwards
-	translation += global_transform.basis.z * speed * delta
+	translation -= global_transform.basis.z * speed * delta
+
+#	print(translation)
+	pass
 
 func _on_Bullet_body_entered(body):
 	if body.has_method("take_damage"):
+		print("hit")
 		body.take_damage(damage)
 		destroy()
 
